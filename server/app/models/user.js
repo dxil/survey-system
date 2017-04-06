7216394edfe09287675fbda01934a6065749a9d2
cuzz <cuzz@xqopen.cn>
2017-04-06 19:39:02 +0800

    no change

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt-nodejs');
 var SALT_WORK_FACTOR = 10;
 
 var user = new Schema({
